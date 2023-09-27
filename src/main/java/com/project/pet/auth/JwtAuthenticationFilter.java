2e532877a6ff9e7dfd65269f48c528742b8d6768
KimSeungjin <whffkaos007@naver.com>
2023-09-27 14:00:48 +0900

    feat : jwt와 security config 구현

--- src/main/java/com/project/pet/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/project/pet/auth/JwtAuthenticationFilter.java
@@ -4,13 +4,36 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+/**
+ * jwt 인증 관련 필터이다. GenericFilterBean를 상속받아도 된다. GenericFilterBean는 기존 필터에서 가져올 수 없는 스프링의 설정 정보를 가져올 수 있게 확장된 추상 클래스이다.
+ * 서블릿은 사용자 요청을 받으면 서블릿을 생성하여 메모리 저장하고 동일한 클라이언트 요청을 받으면 재활용
+ * 하는 구조여서 GenericFilterBean를 상속받으면 RequestDispatcher에 의해 다른 서블릿으로 디스패치되면서
+ * 필터가 두 번 호출되는 현상이 발생할 수 있다고 한다. 이 문제를 해결하기 위해 OncePerRequestFilter를 사용!
+ * OncePerRequestFilter는 GenericFilterBean를 상속받아 구현한 필터이다.
+ */
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+    private final JwtTokenProvider jwtTokenProvider;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response
+            , FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(request);
+        // Authorization 헤더에 있는 jwt 가져오기
+        if(token != null && jwtTokenProvider.validateToken(token)){
+            //토큰이 존재하면서 유효하다면 Authentication 객체 생성
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            //시큐리티 컨텍스트 홀더에 Authentication 저장
+        }
+        filterChain.doFilter(request,response);
+        //doFilter는 서블릿 실행 메서드로 해당 메소드 기준으로 서블릿 실행 전후 로직
     }
 }
--- src/main/java/com/project/pet/common/config/SecurityConfig.java
+++ src/main/java/com/project/pet/common/config/SecurityConfig.java
@@ -1,38 +1,51 @@
-package com.project.pet.common.config;
+    package com.project.pet.common.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+    import com.project.pet.auth.JwtAuthenticationFilter;
+    import com.project.pet.auth.JwtTokenProvider;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+    import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+    import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+    import org.springframework.security.config.http.SessionCreationPolicy;
+    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+    import org.springframework.security.crypto.password.PasswordEncoder;
+    import org.springframework.security.web.SecurityFilterChain;
+    import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+    import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
-@Configuration
-@EnableWebSecurity
-//@EnableGlobalMethodSecurity(securedEnabled = true)
-public class SecurityConfig {
-    private final String[] allowdUrls = { "", "/", "api/ping", "/sign-in", "/sing-up"};
-    @Bean // BCryptPasswordEncoder가 제공하는 암호화로 비밀번호와 같은 중요 정보 노출을 막아준다.
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    @Configuration
+    @EnableWebSecurity
+    @RequiredArgsConstructor
+    //@EnableGlobalMethodSecurity(securedEnabled = true)
+    public class SecurityConfig {
+        private final String[] allowdUrls = { "", "/", "api/ping", "/sign-in", "/sing-up"};
+        private final JwtTokenProvider jwtTokenProvider;
+        /** 따로 jwt security를 분리하려다가 jwtFilter만 추가하므로 통합 filter 설정으로 결정
+         */
+
+        @Bean // BCryptPasswordEncoder가 제공하는 암호화로 비밀번호와 같은 중요 정보 노출을 막아준다.
+        public PasswordEncoder passwordEncoder(){
+            return new BCryptPasswordEncoder();
+        }
+
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            return http
+                    .csrf(csrf -> csrf.disable()) // token 기반, stateless 특성으로 csrf 에 노출될 확률이 매우 낮다. 하지만 쿠키와 같은 기능을 사용한다면 따로 설정해줄 필요는 있음.
+                    .httpBasic(basic -> basic.disable()) // 로그인폼형식 -> authorization 헤더에 아이디와 비밀번호를 넣는 위험한 방식이 있다 정도로만 알고 가자.
+                    // Rest API 쓰므로 사용x, HTTP Basic Authentication이 비활성화되고, JWT 또는 다른 인증 방식을 대신 사용
+                    .sessionManagement(session -> session.sessionCreationPolicy(STATELESS)) //session, cookie를 사용하지 않으므로 stateless 설정
+                    .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),  UsernamePasswordAuthenticationFilter.class)
+                    .authorizeHttpRequests(auth ->
+                            auth.requestMatchers(allowdUrls).permitAll()
+                                    .anyRequest().authenticated()
+                    )
+                    .build();
+        }
     }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf.disable()) // token 기반, stateless 특성으로 csrf 에 노출될 확률이 매우 낮다. 하지만 쿠키와 같은 기능을 사용한다면 따로 설정해줄 필요는 있음.
-                .httpBasic(basic -> basic.disable()) // 로그인폼형식 -> authorization 헤더에 아이디와 비밀번호를 넣는 위험한 방식이 있다 정도로만 알고 가자.
-                // Rest API 쓰므로 사용x, HTTP Basic Authentication이 비활성화되고, JWT 또는 다른 인증 방식을 대신 사용
-                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS)) //session, cookie를 사용하지 않으므로 stateless 설정
-                .authorizeHttpRequests(auth ->
-                        auth.requestMatchers(allowdUrls).permitAll()
-                                .anyRequest().authenticated()
-                )
-                .build();
-    }
-}
