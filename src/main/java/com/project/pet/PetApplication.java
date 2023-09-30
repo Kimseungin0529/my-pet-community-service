3353a196fd6dd9a03df7eff4372550366a0be487
Kimseungin0529 <whffkaos007@naver.com>
2023-09-30 23:43:09 +0900

    test

--- src/main/java/com/project/pet/PetApplication.java
+++ src/main/java/com/project/pet/PetApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetApplication {
 
 	public static void main(String[] args) {
-		//commit  test
+		//commit  test 2222
 		SpringApplication.run(PetApplication.class, args);
 	}
 
