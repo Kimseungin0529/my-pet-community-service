f0f855977ca2544c4623cb8f942d515f083c8e10
Kimseungin0529 <whffkaos007@naver.com>
2023-10-06 14:38:09 +0900

    refactor : 불필요한 필드 삭제
    @Valid 관련 예외 처리 reponse를 분리하여 사용하지 않음

--- src/main/java/com/project/pet/global/common/dto/ErrorResponse.java
+++ src/main/java/com/project/pet/global/common/dto/ErrorResponse.java
@@ -13,18 +13,10 @@ public class ErrorResponse {
     private int errorCode;
     private String errorType;
     private String detail;
-    @JsonInclude(value = JsonInclude.Include.NON_EMPTY) // null이 아니고 비어있지 않는 경우만 json으로 반환
-    private List<String> errors;
 
     public ErrorResponse(CustomException customException) {
         this.errorCode = customException.getErrorCode();
         this.errorType = customException.getErrorType().toString();
         this.detail = customException.getDetail();
-    }
-    public ErrorResponse(CustomException customException, List<String> errors) { // DtoNotValidException 용 글로벌 예외이므로 따로 처리
-        this.errorCode = customException.getErrorCode();
-        this.errorType = customException.getErrorType().toString();
-        this.detail = customException.getDetail();
-        this.errors = errors;
     }
 }
