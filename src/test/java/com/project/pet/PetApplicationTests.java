006607e95158fc77b1281f770b778313198bf300
Kimseungin0529 <whffkaos007@naver.com>
2023-10-31 17:41:39 +0900

    오류 해결 중

--- src/test/java/com/project/pet/PetApplicationTests.java
+++ src/test/java/com/project/pet/PetApplicationTests.java
@@ -1,5 +1,6 @@
 package com.project.pet;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class PetApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(true);
 	}
 
 }
