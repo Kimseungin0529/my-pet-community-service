2acdcf09980b6e37e1a31b2027cee4c914d35a26
Kimseungin0529 <whffkaos007@naver.com>
2023-12-28 00:24:51 +0900

    chore : Dockerfile 생성 + security permitUrl 통합

--- src/main/java/com/project/pet/global/config/SecurityConfig.java
+++ src/main/java/com/project/pet/global/config/SecurityConfig.java
@@ -21,6 +21,7 @@
     @EnableGlobalMethodSecurity(securedEnabled = true)
     public class SecurityConfig {
 
+        private final static String[] PERMIT_API = {"/api/users/sign-up", "/api/users/sign-in", "/api/users/reissue","/", ""};
         private final JwtTokenProvider jwtTokenProvider;
         /** 따로 jwt security를 분리하려다가 jwtFilter만 추가하므로 통합 filter 설정으로 결정
          */
@@ -35,7 +36,7 @@
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
             return http
                     .csrf(csrf -> csrf.disable()) // token 기반, stateless 특성으로 csrf 에 노출될 확률이 매우 낮다. 하지만 쿠키와 같은 기능을 사용한다면 따로 설정해줄 필요는 있음.
-                    .httpBasic(basic -> basic.disable()) // 로그인폼형식 -> authorization 헤더에 아이디와 비밀번호를 넣는 위험한 방식이 있다 정도로만 알고 가자.
+                    .httpBasic(basic -> basic.disable())// 로그인폼형식 -> authorization 헤더에 아이디와 비밀번호를 넣는 위험한 방식이 있다 정도로만 알고 가자.
                     // Rest API 쓰므로 사용x, HTTP Basic Authentication이 비활성화되고, JWT 또는 다른 인증 방식을 대신 사용
                     .sessionManagement(session -> session.sessionCreationPolicy(STATELESS)) //session, cookie를 사용하지 않으므로 stateless 설정
                     .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),  UsernamePasswordAuthenticationFilter.class)
@@ -47,10 +48,7 @@
                      * 이전에 사용자 정의 필터인 JwtAuthenticationFilter에서 인증 및 권한처리가 필요했다.
                      */
                     .authorizeHttpRequests(auth ->
-                            auth.requestMatchers("/api/users/sign-up").permitAll()
-                                    .requestMatchers("/api/users/sign-in").permitAll()
-                                    .requestMatchers("/api/users/reissue").permitAll()
-                                    .requestMatchers("/", "").permitAll()
+                            auth.requestMatchers(PERMIT_API).permitAll()
                                     .anyRequest().authenticated()
                     )
                     .build();
